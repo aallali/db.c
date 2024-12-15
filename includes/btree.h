3ea16c70cd7763303b395a8070a2ff0af7f00131
3dida <44850191+aallali@users.noreply.github.com>
2024-12-15 02:06:44 +0100

    Feat: add level order traversal for BST

--- includes/btree.h
+++ includes/btree.h
@@ -6,7 +6,7 @@
 /*   By: aallali <hi@allali.me>                   ██  █████  █████    _██     */
 /*                                                ██ _____█ _____█   _██      */
 /*   Created: 2024/12/13 13:37:42 by aallali      ██ ██████ ██████   ██.ma    */
-/*   Updated: 2024/12/15 01:05:58 by aallali      -------- 1337.ma -------    */
+/*   Updated: 2024/12/15 01:23:52 by aallali      -------- 1337.ma -------    */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,7 @@ btree *bt_find(int target, btree *startNode);
 bool  bt_insert_node(btree **nodePtr, int v);
 
 int bt_calculate_height(btree *node);
+
+void bt_lvl_order_traverse(btree *node);
 
 #endif // BTREE_H
