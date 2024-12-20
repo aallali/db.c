d782374622002b317175bc54e31638159710c95d
3dida <44850191+aallali@users.noreply.github.com>
2024-12-21 00:22:24 +0100

    [FEAT] Add `avl_create_node` method

---
+++ includes/avl.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*   Copyright 2024 <Abdellah ALLALI>                                         */
+/*                                                                            */
+/*   File    : avl.h                                                          */
+/*   Project : DB.c                                                           */
+/*   License : Apache 2.0 with Commons Clause. See LICENSE file.              */
+/*                                                                            */
+/*   Created: 2024/12/20 12:55:28 by Abdellah A.                              */
+/*   Updated: 2024/12/21 00:05:29 by Abdellah A.                              */
+/* ************************************************************************** */
+
+#ifndef AVL_H
+#define AVL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct AVL_NODE
+{
+    int value;
+    int height;
+    struct AVL_NODE *left;
+    struct AVL_NODE *right;
+} AVL_NODE;
+
+AVL_NODE *avl_create_node(int value);
+
+void avl_main_test();
+
+#endif // AVL_H
---
+++ src/avl.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*   Copyright 2024 <Abdellah ALLALI>                                         */
+/*                                                                            */
+/*   File    : avl.c                                                          */
+/*   Project : DB.c                                                           */
+/*   License : Apache 2.0 with Commons Clause. See LICENSE file.              */
+/*                                                                            */
+/*   Created: 2024/12/20 02:09:52 by Abdellah A.                              */
+/*   Updated: 2024/12/21 00:18:49 by Abdellah A.                              */
+/* ************************************************************************** */
+
+/*
+
+The AVL tree is named after its inventors, Georgy Adelson-Velsky and Evgenii
+Landis, who published it in their 1962 paper “An algorithm for the organization
+of information”.
+
+An AVL tree defined as a self-balancing Binary Search Tree (BST) where the
+difference between heights of left and right subtrees for any node cannot be
+more than one.
+
+*/
+
+#include "avl.h"
+#include "helpers.h"
+
+AVL_NODE *avl_create_node(int value)
+{
+    AVL_NODE *node = (AVL_NODE *)malloc(sizeof(AVL_NODE));
+    node->value = value;
+    node->height = 0;
+    node->left = node->right = NULL;
+    return node;
+}
+
+/*
+ * Example of an AVL Tree Node:
+ *
+ *         20 (Height = 2)
+ *        /  \
+ *       10  30 (Height = 1)
+ *      /  \
+ *     5   15
+ *
+ * Key Details:
+ * - Node "20" is the root with height 2.
+ * - Node "10" has a height of 1 and is balanced.
+ * - Node "30" is a leaf node with height 1.
+ * - Left subtree: 10 → 5, 15 (balanced).
+ * - Right subtree: 30 (no children).
+ */
+
+void avl_main_test()
+{
+    AVL_NODE *node = avl_create_node(20);
+    node->right = avl_create_node(30);
+    node->left = avl_create_node(10);
+    node->left->left = avl_create_node(5);
+    node->left->right = avl_create_node(15);
+
+    avl_print_tree(node);
+}
