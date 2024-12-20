9101f4a90323e6a342cccb72329af62908b73627
3dida <44850191+aallali@users.noreply.github.com>
2024-12-21 00:25:43 +0100

    [REFACTOR] Extract experiment logic out of main file

--- src/main.c
+++ src/main.c
@@ -6,91 +6,23 @@
 /*   License : Apache 2.0 with Commons Clause. See LICENSE file.              */
 /*                                                                            */
 /*   Created: 2024/12/13 13:37:42 by Abdellah A.                              */
-/*   Updated: 2024/12/19 15:42:56 by Abdellah A.                              */
+/*   Updated: 2024/12/21 00:03:15 by Abdellah A.                              */
 /* ************************************************************************** */
 
 #include "bst.h"
-
-void print_node(bst *node)
-{
-  printf("[%d] ", node->value);
-}
+#include "avl.h"
+#include "helpers.h"
 
 int main()
 {
-  bst *node = NULL;
-  node = bst_create_tree(7);
-
-  int values[] = {7, 5, 12, 3, 6, 9, 15, 1, 4, 8, 10, 17};
-  int i = 0;
-  while (i < (int)(sizeof(values) / sizeof(int)))
-    bst_insert_node(&node, values[i++]);
-
-  bst *last_branch;
-  bst *trgt;
-
-  trgt = bst_find(17, node);
-  last_branch = bst_create_tree(13);
-  trgt->left = last_branch;
+  /* clear console */
+  printf("\e[1;1H\e[2J");
+  printf("*****************************************************************\n");
   /*
-        7
-      /   \
-     5     12
-    / \    /  \
-   3   6   9   15
-  / \     / \   \
- 1   4   8  10  17
-               /
-              13
-└──────7
-       ├──────5
-       │      ├──────3
-       │      │      ├──────1
-       │      │      └──────4
-       │      └──────6
-       └──────12
-              ├──────9
-              │      ├──────8
-              │      └──────10
-              └──────15
-                     └──────17
-                            ├──────13
-*/
-
-  printf("+---------------------------------------------------------+\n");
-  printf("+-------------------- Balanced Binary Tree----------------+\n");
-  printf("+---------------------------------------------------------+\n");
-  print_tree(node);
-
-  bst_delete_node(&node, 7); // The nood head will be 6
-
-  print_tree(node);
-
-  bst_in_order_traversal(node, print_node);
-  // output: [1] [3] [4] [5] [6] [7] [8] [9] [10] [12] [15] [13] [17]
-
-  int targets[] = {1, 3, 5, 7, 8, 15, 17};
-  i = 0;
-  while (i < (int)(sizeof(targets) / sizeof(int)))
-  {
-    int target = targets[i];
-    bst *proc = NULL;
-    bst *suc = NULL;
-
-    printf("\n+------- Target: [%d] --------+\n", target);
-    bst_find_predecessor_successor(node, target, &proc, &suc);
-
-    if (proc != NULL)
-      printf("Predecessor of [%d] is [%d]\n", target, proc->value);
-
-    if (suc != NULL)
-      printf("Successor   of [%d] is [%d]\n", target, suc->value);
-
-    i++;
-  }
-  /*
-  Free the homies
+    UnComment the main func collection you want to run
   */
-  bst_free_tree(&node);
+  // bst_main_test();
+  avl_main_test();
+
   return 0;
 }
