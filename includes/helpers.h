0f824d4a010d79b4d55284af735513451b28ab65
3dida <44850191+aallali@users.noreply.github.com>
2024-12-22 02:11:38 +0100

    [REFACTOR] Extract node handlers out of Helpers.c file

--- includes/helpers.h
+++ includes/helpers.h
@@ -6,7 +6,7 @@
 /*   License : Apache 2.0 with Commons Clause. See LICENSE file.              */
 /*                                                                            */
 /*   Created: 2024/12/20 15:17:07 by Abdellah A.                              */
-/*   Updated: 2024/12/21 00:12:41 by Abdellah A.                              */
+/*   Updated: 2024/12/22 01:09:54 by Abdellah A.                              */
 /* ************************************************************************** */
 
 #ifndef HELPERS_H
@@ -17,7 +17,18 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct
+{
+    void *(*get_child)(void *node, bool is_left);
+    char *(*get_value)(void *node);
+} node_handler;
+
 void bst_print_tree(BST_NODE *node);
 void avl_print_tree(AVL_NODE *node);
+void print_tree_recursive(
+    char *prefix,
+    void *node,
+    bool is_left,
+    node_handler *handler);
 
 #endif //  HELPERS_H
