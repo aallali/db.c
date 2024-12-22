b452425ef46bc076ec156ca9c6c52d387a759af4
3dida <44850191+aallali@users.noreply.github.com>
2024-12-22 23:48:29 +0100

    [FIX] Memory leaks @`test_crud_avl.c`

--- tests/test_crud_avl.c
+++ tests/test_crud_avl.c
@@ -6,7 +6,7 @@
 /*   License : Apache 2.0 with Commons Clause. See LICENSE file.              */
 /*                                                                            */
 /*   Created: 2024/12/22 03:33:28 by Abdellah A.                              */
-/*   Updated: 2024/12/22 03:33:29 by Abdellah A.                              */
+/*   Updated: 2024/12/22 16:43:20 by Abdellah A.                              */
 /* ************************************************************************** */
 
 #include "test.h"
@@ -18,6 +18,7 @@ TEST_CASE(test_avl_create_tree)
     TEST_EQUAL(root_node->value, 10);
     TEST_EQUAL(root_node->left, NULL);
     TEST_EQUAL(root_node->right, NULL);
+    avl_free_tree(root_node);
 }
 
 TEST_CASE(test_avl_insert_node)
@@ -28,6 +29,7 @@ TEST_CASE(test_avl_insert_node)
     TEST_EQUAL(root_node->value, 10);
     TEST_EQUAL(root_node->left->value, 1);
     TEST_EQUAL(root_node->right->value, 11);
+    avl_free_tree(root_node);
 }
 
 TEST_CASE(test_avl_insert_node_2)
@@ -44,6 +46,7 @@ TEST_CASE(test_avl_insert_node_2)
     TEST_EQUAL(root_node->left->left->value, 10);
     TEST_EQUAL(root_node->left->right->value, 25);
     TEST_EQUAL(root_node->right->right->value, 50);
+    avl_free_tree(root_node);
 }
 
 TEST_CASE(test_avl_insert_node_3)
@@ -66,5 +69,5 @@ TEST_CASE(test_avl_insert_node_3)
     TEST_EQUAL(root_node->right->left->value, 35);
     TEST_EQUAL(root_node->right->right->right->value, 55);
     TEST_EQUAL(root_node->right->right->left->value, 45);
+    avl_free_tree(root_node);
 }
-
