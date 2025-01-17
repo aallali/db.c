8c7de872f8d275e201e0555fcdaf31c039f8bd42
3dida <44850191+aallali@users.noreply.github.com>
2025-01-17 23:01:46 +0100

    refact: use `meta-header` nvim plugin to generate header

---
+++ hdr_meta.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export HDR_AUTHOR="Copyright © 2024 <hi@allali.me>"
+export HDR_USERNAME="aallali"
+export HDR_PROJECT="DB.c"
+export HDR_LICENSE="Apache 2.0 with Commons Clause. See LICENSE file."
+
+echo "AUTHOR: $HDR_AUTHOR"
+echo "USERNAME: $HDR_USERNAME"
+echo "PROJECT: $HDR_PROJECT"
+echo "LICENSE: $HDR_LICENSE"
+
